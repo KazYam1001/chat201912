702fbb6c73e524b3ef9670bcd1f62947cc38916f
Kazushi Yamanishi <keironia187149@gmail.com>
2019-12-23 12:16:09 +0900

    rename database
    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "ya", email: "a@a", password: "11111111", password_confirmation: "11111111")
+User.create!(name: "yama", email: "a@b", password: "11111111", password_confirmation: "11111111")
+User.create!(name: "yamani", email: "a@c", password: "11111111", password_confirmation: "11111111")
+User.create!(name: "yamanishi", email: "a@d", password: "11111111", password_confirmation: "11111111")
+Group.create!(name: "group1")
+Group.create!(name: "group2")
+Member.create!(user_id: 1, group_id: 1)
+Member.create!(user_id: 2, group_id: 1)
+Member.create!(user_id: 1, group_id: 2)
+Member.create!(user_id: 2, group_id: 2)
